ac8989d5fffe1de28798c36b3634a7aa80f6924a
rajdan1 <nytkoszymon@interia.pl>
2016-03-19 20:30:42 +0100

    IMethodHolder.php for ImplementAlpha

---
+++ ksiazka/IMethodHolder.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ubuntu
+ * Date: 19.03.16
+ * Time: 16:18
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: ubuntu
+ * Date: 19.03.16
+ * Time: 16.30
+ * abstract class
+ */
+interface IMethodHolder
+{
+    function getInfo($info);
+    function sendInfo($info);
+    function calculate($first, $second);
+}
+
+?>
+
+
